f089153bbe6113927cf99d74e7879d5c20f4e063
MidnightArrow <115182293+MidnightArrowStudios@users.noreply.github.com>
2024-12-05 15:26:24 -0500

    Update apply_uv_map_from_library.py

--- brews/apply_uv_map_from_library.py
+++ brews/apply_uv_map_from_library.py
@@ -1,62 +1,81 @@
-"""Sample file showing how to apply a UV map from a DSF file to a geometry asset.
+"""Sample file showing how to apply a UVSet asset from a DSF file.
 
-This code assumes we have already loaded a mesh from a DSF file using the
-"create_geometry_from_library.py" sample file.
+Prerequisites:
+    -run "create_geometry_from_library.py"
 """
 
-# Import packages
-import bpy
-import dufman
+# ============================================================================ #
+# IMPORT                                                                       #
+# ============================================================================ #
 
-# Import datatypes
+# bpy
+import bpy
 from bpy.types import Float2Attribute, IntAttribute, Mesh
-from dufman.structs.uv_set import DsonUVSet
+
+# mathutils
 from mathutils import Vector
 
-# Replace these.
-# - content_directory should match the user's Daz Studio set-up.
-# - uv_set_url should point to a DSF (not DUF) file containing 
-#     a UV set.
-# - daz_geometry should be the name of an already-imported
-#     Daz Studio asset. This example assumes we are reusing the
-#     Genesis 8 Female mesh from "create_geometry_from_library.py".
-DIRECTORY:str = "C:/Users/Public/Documents/Daz3D"
-FILEPATH:str = "/data/DAZ 3D/Genesis 8/Female/UV Sets/DAZ 3D/Base/Base Female.dsf#Base Female"
-MESH_NAME:str = "geometry"
+# dufman
+from dufman import file
+from dufman.structs import DsonUVSet
 
-# Add user's content directory.
-dufman.file.add_content_directory(DIRECTORY)
+# ============================================================================ #
+# FUNCTION                                                                     #
+# ============================================================================ #
 
-# Get the mesh from the Daz Studio asset we already loaded.
-mesh:Mesh = bpy.data.meshes[MESH_NAME]
-
-# Extract UV map data from DSF file.
-struct:DsonUVSet = dufman.create.uv_set.create_uv_set_struct(FILEPATH)
-
-if not struct.expected_vertices == len(mesh.vertices):
-    raise Exception("Could not apply UV map. Mismatched vertex count.")
-
-# Create mesh attribute to store UV map data.
-uv_map:Float2Attribute = mesh.attributes.new(struct.library_id, 'FLOAT2', 'CORNER')
-
-# In "create_geometry_from_library.py", we cached the original
-#   face indices. Now we extract them, in case the geometry is bad.
-face_indices:IntAttribute = mesh.attributes["dson_face_indices"]
-
-# Loop through all faces and add UV coordinates to face corners.
-for (index, polygon) in enumerate(mesh.polygons):
+def apply_uv_map(mesh:Mesh, struct:DsonUVSet) -> Float2Attribute:
+    """Apply a UV map using the data inside a UVSet struct."""
     
-    # Use original DSON index, in case geometry is bad.
-    original_index:int = face_indices.data[index].value
+    # Ensure number of vertices match.
+    if len(mesh.vertices) != struct.expected_vertices:
+        raise Exception("Could not apply UV Map. Vertex count mismatch.")
     
-    # DSON has a bizarre method of handling UVs, which involves
-    #   swapping vertex indices so they can be used to index into 
-    #   the UV coordinates array. This helper function handles
-    #   all of that for us.
-    vertex_indices:list[int] = struct.hotswap_polygon(original_index, polygon.vertices)
+    # The UV map we will insert data into.
+    uv_map:Float2Attribute = mesh.attributes.new(struct.library_id, 'FLOAT2', 'CORNER')
     
-    # Loop through the face's corners and apply the UV 
-    #   coordinates from from the UV set struct.
-    for (iteration, uv_index) in enumerate(vertex_indices):
-        loop_index:int = polygon.loop_start + iteration
-        uv_map.data[loop_index].vector = Vector(struct.uv_coordinates[uv_index])
+    # These are the cached indices from the DSF file, which we use in case we
+    #   had to skip invalid geometry.
+    face_indices:IntAttribute = mesh.attributes["daz_original_index"]
+    
+    # Add UV coordinates from struct.
+    for (index, polygon) in enumerate(mesh.polygons):
+    
+        # Get the original index, cached from the DSF file.
+        original_index:int = face_indices.data[index].value
+        
+        # DSON has a bizarre way of storing vertex indices. This helper method
+        #   handles the confusing part for us.
+        vertex_indices:list[int] = struct.hotswap(original_index, polygon.vertices)
+    
+        for (iteration, uv_index) in enumerate(vertex_indices):
+            loop_index:int = polygon.loop_start + iteration
+            uv_map.data[loop_index].vector = Vector(struct.uv_coordinates[uv_index])
+    
+    return uv_map
+
+
+# ============================================================================ #
+# SCRIPT                                                                       #
+# ============================================================================ #
+
+# Change this to match Daz Studio install directory
+CONTENT_DIRECTORY:str = "C:/Users/Public/Documents/Daz3D"
+UV_SET_URL:str = "/data/DAZ 3D/Genesis 8/Female/UV Sets/DAZ 3D/Base/Base Female.dsf#Base Female"
+
+# ============================================================================ #
+
+# Setup
+file.add_content_directory(CONTENT_DIRECTORY)
+
+# The mesh we want to assign the UV map to. Should have been created by
+#   running "create_geometry_from_library.py".
+mesh:Mesh = bpy.data.meshes["geometry"]
+
+# Construct intermediate representation using DUFMan.
+struct:DsonUVSet = DsonUVSet.load(UV_SET_URL)
+
+# Assign UV map to mesh.
+apply_uv_map(mesh, struct)
+
+# Cleanup
+file.remove_content_directory(CONTENT_DIRECTORY)
